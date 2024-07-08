83b09de25739ea5e009120451a1e465dd7de5e57
Chris Bennett <110393183+oev-chrisbennett@users.noreply.github.com>
2024-07-08 21:38:36 +0200

    refactor: dummy action

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello, World!')
+import * as core from '@actions/core'
+import * as github from '@actions/github'
+
+/** Retrieves and outputs the github PR title
+ * @returns void
+ * */
+const run = async (): Promise<void> => {
+    try {
+        const prtitle = github.context.payload.pull_request?.title
+        core.setOutput('pr-title', prtitle)
+    } catch (error) {
+        core.setFailed((error as Error).message)
+    }
+}
+
+run()
